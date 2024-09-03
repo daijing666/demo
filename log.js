9e77e058faa1231f02289f67e10620e2dee3b6f9
Daijing <minamiplus@outlook.com>
2024-09-03 16:12:14 +0900

    2

--- log.js
+++ log.js
@@ -3,7 +3,7 @@ let log4js = require('log4js');
 log4js.configure('./log4js.json');
 log4js.level = 'DEBUG';
 
-//log
+//log-zx
 module.exports = async (ctx, next) => {
     let accessLog = log4js.getLogger('access');
     accessLog.trace(`${ctx.method} ${ctx.url}`);
